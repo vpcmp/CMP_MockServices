de576192305e818dbf375fa11ff2573adab9c2a2
vpcmp <109022811+vpcmp@users.noreply.github.com>
2022-07-13 08:21:33 +0700

    Create Customer Information Response
    Create Customer Information Response

---
+++ Cmp_MockServices/Cmp_MockServices/src/main/java/com/vpbank/cmp/mockservices/CmpMockServicesApplication.java
@@ -0,0 +1,13 @@
+package com.vpbank.cmp.mockservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmpMockServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmpMockServicesApplication.class, args);
+    }
+
+}
---
+++ Cmp_MockServices/Cmp_MockServices/src/main/java/com/vpbank/cmp/mockservices/service/CustomerService.java
@@ -0,0 +1,5 @@
+package com.vpbank.cmp.mockservices.service;
+
+public interface CustomerService {
+    String getCustomerInfo(String cif);
+}
