c8bdf79b518727098e859cb6bd80319ce2e8968a
nmbinh <bombum270389@gmail.com>
2022-07-19 16:50:37 +0700

    Add Payment Requests

---
+++ Cmp_MockServices/src/main/java/com/vpbank/cmp/mockservices/controller/paymentController/TransactionInfoController.java
@@ -0,0 +1,24 @@
+//Account Summary Interface
+package com.vpbank.cmp.mockservices.controller.paymentController;
+
+import com.vpbank.cmp.mockservices.service.paymentServices.TransactionInfoServices;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/cas/cashs/payments/transfers/list")
+@RequiredArgsConstructor
+public class TransactionInfoController {
+
+    private final TransactionInfoServices transactionInfoServices;
+
+    @GetMapping
+    String getTransactionInfoServices(@RequestParam("debitAccount") String debAcc,
+                                      @RequestParam("refNo") String refNo) {
+        return transactionInfoServices.getTransactionInfoServices(debAcc, refNo);
+    }
+
+}
